9b49d906f2d58e06570d6c02128e1fe17fd66934
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2023-09-28 10:58:09 +0900

    追加ボタン押下でリストに追加して表示されるように変更

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,41 @@ interface TodoItem{
 }
 
 function Todo(){
-  const [todos,setTodos] = useState<TodoItem[]>([
-    {
-      task: 'りんご',
-      isCompleted: false,
-    },
-  ]);
     return(
       <div>
       <h1>To Do List</h1>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo.task}</li>
-        ))}
-      </ul>
     </div>
     );
   }
 
 function AddTask(){
+  const [todos,setTodos] = useState<TodoItem[]>([]);
+  const [task, setTask] = useState<string>('');
+
+const handleNewTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setTask(event.target.value);
+  console.log(event.target.value)
+}
+
+const handleClick = (event : React.MouseEvent<HTMLButtonElement>) => {
+  if(task === '') return;
+  setTodos((todos) => [...todos,{task,isCompleted:false}]);
+  setTask('');
+}
   return(
-    <>
-    Add Task : <input placeholder='Add New Task' />
-    </>
+    <div>
+        <div>
+        Add Task : <input placeholder='Add New Task' value={task} onChange={handleNewTask} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>追加</button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo.task}</li>
+          ))}
+        </ul>
+      </div>
   )
 }
 
