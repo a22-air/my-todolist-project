0f3efa27baed2a0fca9319cf303ec5d0cceb3df9
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2023-10-05 11:36:30 +0900

    AddTaskを別ファイルで管理

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {useState,useEffect,useRef} from 'react';
 import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-community/async-storage';
 import Linkify from 'linkify-react';
+import { AddTask } from './AssTask';
 
 //ストレージの作成
 const storage: Storage = new Storage({
@@ -34,72 +35,6 @@ function Todo(){
     </div>
     );
   }
-
-// AddTaskコンポーネント　========================================================
-
-// 追加ボタン押下時に発動する関数
-function AddTask(){
-
-  const [task, setTask] = useState<string>('');
-
-// テキストをセットする関数
-const handleNewTask = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setTask(event.target.value);
-  console.log(event.target.value)
-}
-
-  // 追加ボタンでデータの追加をする関数
-  const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    // テキストが空だったら以下の処理は行わない
-    if (task === '') return;
-    //taskにテキストに入力されたデータをセットする
-    setTask('');
-
-    try {
-      // 既存のデータを読み込む
-      const existingData = await storage.load({
-        key: 'keyWord',
-      });
-
-      let updatedData: { col1: string[] } = { col1: [] };
-      // 既存のデータがあれば、それを取得し新しいデータを追加
-      if (existingData) {
-        updatedData = {
-          ...existingData,
-          col1: [...existingData.col1, task],
-        };
-      } else {
-        // 既存のデータがない場合、新しいデータを作成
-        updatedData.col1 = [task];
-      }
-
-      // 新しいデータを保存
-      await storage.save({
-        key: 'keyWord',
-        data: updatedData,
-      });
-
-      window.location.reload(); // ページをリロードする
-
-    } catch (err) {
-      console.log('エラー:', err);
-    }
-  };
-
-  return(
-    <div>
-        <div>
-        Add Task : <input placeholder='Add New Task' onChange={handleNewTask}
-        value={task}
-        />
-        </div>
-        <div>
-          <button onClick={handleClick}>追加</button>
-        </div>
-      </div>
-  )
-}
-//======================================================================================
 
 // AddTextコンポーネント　=================================================================
 // 追加されたデータを画面に表示するコンポーネント
