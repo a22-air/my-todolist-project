590cb21c4d6ed66ac3e2ee6be84e76de12a6ee95
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2023-10-27 10:43:57 +0900

    モーダルのfooter変更

--- src/LabelList.tsx
+++ src/LabelList.tsx
@@ -94,6 +94,8 @@ type LabelListProps = {
             });
       });
 
+
+
     return(
         <>
         {showModal ? (
@@ -168,21 +170,22 @@ type LabelListProps = {
 
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
+
+                  <button
+                    className="bg-red-400 text-white active:bg-emerald-600 font-bold uppercase text-sm px-4 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {setShowModal(false);
+                    }}
+                  >
+                    ラベル編集
+                  </button>
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => {setShowModal(false);
-                    }}
-                  >
-                    追加
                   </button>
                 </div>
               </div>
