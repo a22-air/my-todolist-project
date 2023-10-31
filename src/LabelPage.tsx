8492c462070e10942f51ac44986c550d6bc4a268
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2023-10-31 11:16:49 +0900

    選択されたラベルのデータを表示する処理

--- src/LabelPage.tsx
+++ src/LabelPage.tsx
@@ -2,19 +2,40 @@ import React, { useEffect,useState,useContext } from "react";
 import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-community/async-storage';
 import { MyContext } from "./App";
+import { NearMeDisabledSharp } from "@mui/icons-material";
 
 export const useMyContext = () => {
     return useContext(MyContext);
   };
 export function LabelPage(){
-        const { updatedData, selectLabel,selectData } = useMyContext();
+    const { updatedData, selectLabel,selectData } = useMyContext();
+
+    const indices = updatedData.col4
+        .map((subArray, index) => subArray.includes(selectData) ? index : -1)
+        .filter((index) => index !== -1);
+        console.log(indices);
+
+        const selectedData = indices.map(index => ({
+            col1: updatedData.col1[index],
+            col2: updatedData.col2[index],
+            col3: updatedData.col3[index],
+            col4: updatedData.col4[index],
+          }));
+
+          console.log('selectedData'+JSON.stringify(selectedData));
+
+
 
     return (
         <>
-         <h2>{updatedData.col1[selectLabel] + updatedData.col2[selectLabel] + updatedData.col3[selectLabel]}</h2>
-         <p>{selectLabel}</p>
-         <p>{updatedData.col1}</p>
-         <p>{selectData}</p>
+         <h3>{selectData}</h3>
+         {selectedData.map((data, index) => (
+            <div key={`selectData_${index}`}>
+              <p>{data.col1}</p>
+              <p>{data.col2}</p>
+              <p>{data.col3}</p>
+            </div>
+        ))}
         </>
 
     )
